dda01107bc96773ab0cdb232ca84a9187bab45a7
asherliu <asherliu@gwu.edu>
2016-10-03 16:13:49 -0400

    header

--- tuple_text_to_binary_csr/tuple_text_to_bin.cpp
+++ tuple_text_to_binary_csr/tuple_text_to_bin.cpp
@@ -9,7 +9,23 @@
  * 
  * Please cite the following paper:
  * 
- * Hang Liu and H. Howie Huang. 2015. Enterprise: breadth-first graph traversal on GPUs. In Proceedings of the International Conference for High Performance Computing, Networking, Storage and Analysis (SC '15). ACM, New York, NY, USA, , Article 68 , 12 pages. DOI: http://dx.doi.org/10.1145/2807591.2807594
+ * Hang Liu and H. Howie Huang. 2015. Enterprise: breadth-first graph traversal on GPUs. In Proceedings of the International Conference for High Performance Computing, Networking, Storage and Analysis (SC '15). ACM, New York, NY, USA, Article 68 , 12 pages. DOI: http://dx.doi.org/10.1145/2807591.2807594
+ 
+ *
+ * This file is part of Enterprise.
+ *
+ * Enterprise is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Enterprise is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Enterprise.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <iostream>
 #include <unistd.h>
